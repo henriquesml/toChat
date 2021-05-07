dfed1358e1a2717310654a1dce7ec46924a3b08d
henriquesml <henrique_schmeller@hotmail.com>
2021-05-07 17:45:29 -0300

    chore: adição do jest no backend

---
+++ back-end/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  clearMocks: true,
+  preset: 'ts-jest',
+  roots: ['<rootDir>/src'],
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '!**/index.ts'
+  ]
+}
