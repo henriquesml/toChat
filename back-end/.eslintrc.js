083d5955e8aa981e1f8fd693ff008ea80ccf0961
henriquesml <henrique_schmeller@hotmail.com>
2021-05-09 20:17:13 -0300

    chore: ajuste eslint backend

--- back-end/.eslintrc.js
+++ back-end/.eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   ],
   rules: {
     'space-before-function-paren': ['error', 'never'],
-    'no-useless-constructor': 'off'
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-var-requires': 'off'
   }
 }
