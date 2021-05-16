a9efb1ae4780b8792caae17fc7b95343540c034f
henriquesml <henrique_schmeller@hotmail.com>
2021-05-16 10:42:14 -0300

    chore: adição do gun no backend

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,9 +1,12 @@
 import 'module-alias/register'
 import { MongoHelper } from '@/infra/repository'
+const Gun = require('gun')
 
 MongoHelper.connect(String(process.env.MONGO_URI))
   .then(async() => {
     const app = (await import('./main/config/app')).default
-    app.listen(3333, () => console.log('Server running at http://localhost:3333'))
+    app.use(Gun.serve)
+    const server = app.listen(3333, () => console.log('Server running at http://localhost:3333'))
+    Gun({ web: server })
   })
   .catch(console.error)
