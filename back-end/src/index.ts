3a04ad2f0b35bb1b6d292d7b609048ee2858d315
henriquesml <henrique_schmeller@hotmail.com>
2021-05-17 17:35:48 -0300

    ajuste porta backend

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,11 +2,13 @@ import 'module-alias/register'
 import { MongoHelper } from '@/infra/repository'
 const Gun = require('gun')
 
+const port = process.env.PORT || 3333
+
 MongoHelper.connect(String(process.env.MONGO_URI))
   .then(async() => {
     const app = (await import('./main/config/app')).default
     app.use(Gun.serve)
-    const server = app.listen(3333, () => console.log('Server running at http://localhost:3333'))
+    const server = app.listen(port, () => console.log('Server running at http://localhost:3333'))
     Gun({ web: server })
   })
   .catch(console.error)
